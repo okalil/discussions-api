375fe716b9b4eb02a967008b8320edae2869c7a7
Andrew Kalil <kalilmagal@gmail.com>
2023-07-19 16:16:22 -0300

    add healthcheck

--- start/routes.ts
+++ start/routes.ts
@@ -19,9 +19,11 @@
 */
 
 import Route from '@ioc:Adonis/Core/Route'
+import HealthCheck from '@ioc:Adonis/Core/HealthCheck'
 
-Route.get('/', async () => {
-  return { hello: 'world' }
+Route.get('/', async ({ response }) => {
+  const report = await HealthCheck.getReport()
+  return report.healthy ? response.ok(report) : response.badRequest(report)
 })
 
 Route.group(() => {
